015b450f85e6cd7e5eb41710ffcd1364d5231bdb
Tyler Cox <tyler.a.cox@berkeley.edu>
2023-12-27 19:20:33 -0700

    Add bot class

---
+++ khet/engine/bot.py
@@ -0,0 +1,16 @@
+"""
+File for handling the game-bot
+"""
+class Player:
+    """
+    """
+    def __init__(self, color, evaluator='naive'):
+        """
+        """
+        self.color = color
+        self.evaluator = evaluator
+
+    def get_move(self, board):
+        """
+        """
+        pass
