a669be4660ee1ba8ebc2fa7d49e68a1c440d14b7
Tyler Cox <tyler.a.cox@berkeley.edu>
2024-01-26 12:32:46 -0800

    add neural network

--- khet/engine/network.py
+++ khet/engine/network.py
@@ -2,20 +2,39 @@ import torch
 from torch import nn
 import torch.optim as optim
 
-def loss_function(state, outcome, network, pi):
+def loss_function(prob, value, outcome, pi):
     """
+    Loss function for the neural network training
+
+    Parameters
+    ----------
+    prob : torch.Tensor
+        The probability distribution over the possible moves
+    value : torch.Tensor
+        The value function for the state
+    outcome : torch.Tensor
+        The outcome of the game
+    pi : torch.Tensor
+        The probability distribution over the possible moves
+
+    Returns
+    -------
+    torch.Tensor
+        The loss function
     """
-    logits = network(state)
-    prob = nn.Softmax(dim=1)(logits[:, :-1])
-    vs = logits[:, -1]
-    return torch.square(outcome - vs).sum() - torch.sum(pi * torch.log(prob))
+    return torch.square(outcome - value).sum() - torch.sum(pi * torch.log(prob))
 
 class NeuralNetwork(nn.Module):
+    """
+    Neural network for the game of Khet
+    """
     def __init__(self):
         """
         """
         super().__init__()
-        self.flatten = nn.Flatten()
+    
+        # Define network layers
+        # TODO: Make convolutional
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(9, 45),
             nn.ReLU(),
@@ -23,18 +42,36 @@ class NeuralNetwork(nn.Module):
             nn.ReLU(),
         )
 
+        # First set of outputs is a probability distribution over the 9 possible
         self.output1 = nn.Sequential(
             nn.Linear(45, 9),
             nn.Softmax(dim=-1)
         )
 
+        # Second set of outputs is a value function for the state
         self.output2 = nn.Sequential(
             nn.Linear(45, 1),
             nn.Tanh()
         )
         
     def forward(self, input):
+        """
+        Compute the forward pass of the neural network
+
+        Parameters
+        ----------
+        input : torch.Tensor
+            The input to the neural network
+        
+        Returns
+        -------
+        torch.Tensor
+            The output of the neural network
+        """
+        # Pass the input through the network
         x = self.linear_relu_stack(input)
+
+        # Return the output
         return self.output1(x), self.output2(x)
 
     
